1a277d4ba6e390c7a35a34a939ceca0b8e05a98a
James Darvell <jdarvell@sur.co.uk>
2015-01-17 19:38:28 +0100

    Imports jquery if not already loaded

--- category_links_as_table.js
+++ category_links_as_table.js
@@ -1,11 +1,26 @@
 (function(){
-var links = ''; 
-jQuery('ul[id^="ref_"] > li> a').not('a[href*=publication_date], li.shoppingEngineExpand > a').each(function(){
-  l = jQuery(this); 
-  links += '<tr><td>' + l.text() + '</td><td>' + l.attr('href') + '</td></tr>\n';
-});
+   if (typeof jQuery=='undefined') { 
+      script = document.createElement( 'script' );
+      script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+      script.onload=buildTable;
+      document.body.appendChild(script);
+   }
+   else {
+      buildTable();
+   }
 
-var table = '<table>\n<thead><tr><th>Category</th><th>Url</th></tr></thead>' + links + '</table>';
+   function buildTable(){
+      var tbody = ''; 
+      jQuery('ul[id^="ref_"] > li> a')
+      .not('a[href*=publication_date], li.shoppingEngineExpand > a')
+      .each(function(){
+         l = jQuery(this); 
+         tbody += '<tr><td>' + l.text() + '</td><td>' + l.attr('href') + '</td></tr>\n';
+      });
 
-jQuery('body').replaceWith(table);
+      var table = '<table>\n<thead><tr><th>Category</th><th>Url</th></tr></thead><tbody>' 
+         + tbody + '</tbody></table>';
+
+      jQuery('body').replaceWith(table);
+   }
 })();
